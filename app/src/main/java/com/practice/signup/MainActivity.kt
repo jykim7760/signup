4a7b44d6169426cb6ed844f3d4437d53b5b315df
jykim7760 <jykim7760@naver.com>
2020-04-08 21:09:55 +0900

    다음버튼 누르면 다른 페이지로 이동

--- app/src/main/java/com/practice/signup/MainActivity.kt
+++ app/src/main/java/com/practice/signup/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.practice.signup
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextBtn.setOnClickListener {
+
+
+            val myIntent = Intent(this, MyPageActivity::class.java)
+            startActivity(myIntent)
+
+
+        }
+
+
     }
 }
---
+++ app/src/main/java/com/practice/signup/MyPageActivity.kt
@@ -0,0 +1,12 @@
+package com.practice.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_page)
+    }
+}
